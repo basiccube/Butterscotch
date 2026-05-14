fdcf624098bd715977298f86283a8b178601e5e3
MrPowerGamerBR <git@mrpowergamerbr.com>
2026-05-13 22:02:40 -0300

    Revert "Scale glBlitFramebuffer via ratio"
    This reverts commit 2af29c12148325bf7cff3665f09d4e057cd78a6c.

--- vendor/ps3gl/ps3gl.c
+++ vendor/ps3gl/ps3gl.c
@@ -1052,67 +1052,39 @@ GLAPI void APIENTRY glBlitFramebuffer (GLint srcX0, GLint srcY0, GLint srcX1, GL
   gcmTransferScale scale;
   gcmTransferSurface surface;
 
-  // Source surface dimensions/offset/pitch (full source surface, NOT the source rect)
-  uint16_t srcSurfW, srcSurfH;
-  uint32_t srcOffset, srcPitch;
-  if (_opengl_state.bound_read_framebuffer != NULL) {
-    srcSurfW  = _opengl_state.bound_read_framebuffer->gcmSurface.width;
-    srcSurfH  = _opengl_state.bound_read_framebuffer->gcmSurface.height;
-    srcOffset = _opengl_state.bound_read_framebuffer->gcmSurface.colorOffset[0];
-    srcPitch  = _opengl_state.bound_read_framebuffer->gcmSurface.colorPitch[0];
-  } else {
-    srcSurfW  = display_width;
-    srcSurfH  = display_height;
-    srcOffset = color_offset[curr_fb^1];
-    srcPitch  = color_pitch;
-  }
-
-  // Destination surface dimensions/offset/pitch
-  uint16_t dstSurfW, dstSurfH;
-  uint32_t dstOffset, dstPitch;
-  if (_opengl_state.bound_draw_framebuffer != NULL) {
-    dstSurfW  = _opengl_state.bound_draw_framebuffer->gcmSurface.width;
-    dstSurfH  = _opengl_state.bound_draw_framebuffer->gcmSurface.height;
-    dstOffset = _opengl_state.bound_draw_framebuffer->gcmSurface.colorOffset[0];
-    dstPitch  = _opengl_state.bound_draw_framebuffer->gcmSurface.colorPitch[0];
-  } else {
-    dstSurfW  = display_width;
-    dstSurfH  = display_height;
-    dstOffset = color_offset[curr_fb];
-    dstPitch  = color_pitch;
-  }
-
-  int32_t srcRectW = srcX1 - srcX0;
-  int32_t srcRectH = srcY1 - srcY0;
-  int32_t dstRectW = dstX1 - dstX0;
-  int32_t dstRectH = dstY1 - dstY0;
-
   scale.conversion = GCM_TRANSFER_CONVERSION_TRUNCATE;
   scale.format = GCM_TRANSFER_SCALE_FORMAT_A8R8G8B8;
   scale.origin = GCM_TRANSFER_ORIGIN_CORNER;
   scale.operation = GCM_TRANSFER_OPERATION_SRCCOPY;
-  scale.interp = (filter == GL_LINEAR) ? GCM_TRANSFER_INTERPOLATOR_LINEAR : GCM_TRANSFER_INTERPOLATOR_NEAREST;
+  scale.interp = GCM_TRANSFER_INTERPOLATOR_NEAREST;
   scale.clipX = 0;
   scale.clipY = 0;
-  scale.clipW = dstSurfW;
-  scale.clipH = dstSurfH;
+  scale.clipW = display_width;
+  scale.clipH = display_height;
   scale.outX = dstX0;
   scale.outY = dstY0;
-  scale.outW = dstRectW;
-  scale.outH = dstRectH;
-  // Scaling: each step in dest space advances ratio source pixels.
-  scale.ratioX = rsxGetFixedSint32((float) srcRectW / (float) dstRectW);
-  scale.ratioY = rsxGetFixedSint32((float) srcRectH / (float) dstRectH);
-  scale.inX = rsxGetFixedUint16((float) srcX0);
-  scale.inY = rsxGetFixedUint16((float) srcY0);
-  scale.inW = srcSurfW;
-  scale.inH = srcSurfH;
-  scale.offset = srcOffset;
-  scale.pitch = srcPitch;
+  scale.outW = dstX1-dstX0;
+  scale.outH = dstY1-dstY0;
+  scale.ratioX = rsxGetFixedSint32(1.f);
+  scale.ratioY = rsxGetFixedSint32(1.f);
+  scale.inX = rsxGetFixedUint16(srcX0);
+  scale.inY = rsxGetFixedUint16(srcY0);
+  scale.inW = srcX1-srcX0;
+  scale.inH = srcY1-srcY0;
+  scale.offset = _opengl_state.bound_read_framebuffer != NULL ? 
+				_opengl_state.bound_read_framebuffer->gcmSurface.colorOffset[0] :
+				color_offset[curr_fb^1];
+  scale.pitch = _opengl_state.bound_read_framebuffer != NULL ? 
+				_opengl_state.bound_read_framebuffer->gcmSurface.colorPitch[0] :
+				color_pitch;
 
   surface.format = GCM_TRANSFER_SURFACE_FORMAT_A8R8G8B8;
-  surface.pitch = dstPitch;
-  surface.offset = dstOffset;
+  surface.pitch = _opengl_state.bound_draw_framebuffer != NULL ? 
+				_opengl_state.bound_draw_framebuffer->gcmSurface.colorPitch[0] :
+				color_pitch;
+  surface.offset = _opengl_state.bound_draw_framebuffer != NULL ?
+				_opengl_state.bound_draw_framebuffer->gcmSurface.colorOffset[0] :
+				color_offset[curr_fb];
 
   rsxSetTransferScaleMode(context, GCM_TRANSFER_LOCAL_TO_LOCAL, GCM_TRANSFER_SURFACE);
   rsxSetTransferScaleSurface(context, &scale, &surface);
